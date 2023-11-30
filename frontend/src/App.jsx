9afc8f1dd262f758577e47ed79df8f8ead1f2233
Hendri Zeneli <hendrizeneli@yahoo.com>
2023-11-30 13:40:01 +0100

    components & pages in src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
+      <Footer />
     </>
   );
 }
