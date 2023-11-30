beec91b6d441e506ba9d1847aeec203295e1b5ef
Ahmad Housh <ahmedadnanhoush@gmail.com>
2023-11-30 13:47:24 +0100

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
       </div>
     </>
   );
