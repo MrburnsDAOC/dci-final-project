c177f0ba1c1baab5f75abceac450f6efa3d1dd2b
David Märkisch <david.maerkisch@gmail.com>
2023-11-30 13:39:03 +0100

    tailwind problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Home from "../pages/home/Home";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Footer className="hover:bg-black text-white" />
+        <Home />
+        <h2 className="bg-slate-600 text-cyan-300">TESTEST</h2>
+        <h2 className="bg-gray-900"> ANOTHER TEST</h2>
+        <h2 className="bg-red-600"> LALALALLALALAL</h2>
+        <h2 className="bg-lime-500">SALEM</h2>
+        <h2 className=" bg-yellow-400 font-bold">FINALLY?</h2>
       </div>
     </>
   );
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../../components/home/Carousel";
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export default Home;
