42e9fda202d54801c1ba17114b9bb431b52e8696
Ahmad Housh <ahmedadnanhoush@gmail.com>
2023-12-01 11:40:41 +0100

    change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+ /** @type {import('tailwindcss').Config} */
+import withMT from "@material-tailwind/react/utils/withMT";
+//const withMT = require("@material-tailwind/react/utils/withMT");
 export default withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
