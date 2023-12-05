6d0c3cc8d10e52828731e37c72ab1ac6e389e691
David Märkisch <david.maerkisch@gmail.com>
2023-12-05 13:41:10 +0100

    Kontakt: build  KontaktData component for implementation in Kontakt page

--- frontend/src/pages/kontakt/Kontakt.jsx
+++ frontend/src/pages/kontakt/Kontakt.jsx
@@ -1,5 +1,31 @@
+import H2 from "../../layout/H2";
+import KontaktData from "../../layout/KontaktData";
+import Section from "../../layout/Section";
+
 const Kontakt = () => {
-  return <div>Kontakt</div>;
+  return (
+    <>
+      <Section>
+        <H2>Kontakt</H2>
+        <div
+          id="coronaHinweis"
+          className="text-red-600  font-semibold my-3 leading-5"
+        >
+          <p>
+            Aufgrund der anhaltenden Corona-Situation sehen wir uns leider
+            gezwungen das Tierheim Dachau bis auf weiteres für Besucher
+            geschlossen zu halten.
+          </p>
+          <p className="mt-3">
+            Tiervermittlungen finden jedoch statt allerdings nur mit
+            terminlicher Absprache.
+          </p>
+        </div>
+        <KontaktData />
+      </Section>
+      <Section></Section>
+    </>
+  );
 };
 
 export default Kontakt;
