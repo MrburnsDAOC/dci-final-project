e08418783e02da5ef70078477b510366398b6b32
David Märkisch <david.maerkisch@gmail.com>
2023-12-07 12:29:44 +0100

    NotFound: created layout

--- frontend/src/pages/notFound/NotFound.jsx
+++ frontend/src/pages/notFound/NotFound.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
+import Section from "../../layout/Section";
 
 const NotFound = () => {
   return (
-    <div className="min-h-[70vh]">
-      <p>404</p>
-      <br />
-      This is NotFound
-      <Link to="/">Go to home Page</Link>
-    </div>
+    <Section>
+      <div className="my-[30vh] text-center">
+        <h6 className=" text-2xl my-4">404: Seite nicht gefunden</h6>
+        <Link to="/">
+          <button className="bg-mainBg border-white border-2 text-secondText p-3 rounded-full">
+            zurück zu Home
+          </button>
+        </Link>
+      </div>
+    </Section>
   );
 };
 
